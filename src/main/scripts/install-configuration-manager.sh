300892150609f4b70880b0e649f39813b6867607
Gordon Force <gordonff@gmail.com>
2025-07-12 07:43:05 -0700

    Documentation updates

--- src/main/scripts/install-configuration-manager.sh
+++ src/main/scripts/install-configuration-manager.sh
@@ -1,40 +1,29 @@
-c#!/usr/bin/env zsh
+#!/usr/bin/bash
 
 
 # If on macOS
 # Step 1: Install a minimal homebrew environment to install git and ansible
 # Get homebrew here
-brew install ansible git # install unattended by answering with default values to questions
+brew install --git ansible git # install unattended by answering with default values to questions
 # or else
 sudo apt-get install -y ansible git  # install unattended by answering with default values to questions
 
 # Step 2: Clone the repository
 # Check if we're already in the repository directory
-mkdir -p $HOME/.dev-env
-cd $HOME/.dev-env
-
+DEV_ENV_LOCAL="$HOME"/.dev-env
+DEV_ENV_LOCAL_PLAYBOOKS_REPO="$DEV_ENV_LOCAL"/dev-env
+mkdir -p "$DEV_ENV_LOCAL"
+cd "$DEV_ENV_LOCAL" || exit 1;
 if [[ ! -e "dev-env" ]]; then
-  git clone
-fi
-
-if [[ ! -d "debian-java-development-environment" && ! -f "src/main/ansible/main.yml" ]]; then
-  git clone https://github.com/gordonforce/debian-java-development-environment
-  cd debian-java-development-environment
-fi
-
-# Step 3: Run the main playbook
-# Determine the correct path to the main.yml file
-if [[ -f "main.yml" ]]; then
-  ansible-playbook main.yml
-elif [[ -f "src/main/ansible/main.yml" ]]; then
-  cd src/main/ansible
-  ansible-playbook main.yml
-  cd - > /dev/null
-elif [[ -f "${HOME}/debian-java-development-environment/src/main/ansible/main.yml" ]]; then
-  cd ${HOME}/debian-java-development-environment/src/main/ansible
-  ansible-playbook main.yml
-  cd - > /dev/null
+  git clone https://github.com/gordonforce/dev-env
 else
-  echo "Error: Could not find main.yml playbook"
-  exit 1
+  cd "$DEV_ENV_LOCAL_PLAYBOOKS_REPO" || exit 2;
+  git pull
 fi
+
+cd "$HOME" || exit 3
+
+ansible -f "$DEV_ENV_LOCAL_PLAYBOOKS_REPO"/src/main/ansible/zsh/playbook.yaml
+ansible -f "$DEV_ENV_LOCAL_PLAYBOOKS_REPO"/src/main/ansible/oh-my-zsh/playbook.yaml
+ansible -f "$DEV_ENV_LOCAL_PLAYBOOKS_REPO"/src/main/ansible/homebrew/playbook.yaml
+ansible -f "$DEV_ENV_LOCAL_PLAYBOOKS_REPO"/src/main/ansible/secrets/playbook.yaml
